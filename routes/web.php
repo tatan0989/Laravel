474c24bf1d5949c925a4758fd0841a7446844154
tatan0989 <estefanyrojas200419@gmail.com>
2025-03-27 09:55:10 -0500

    Rutas

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,27 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    /* return view('welcome'); */
+    return "Bienvenido a la pagina principal";
+});
+Route::get('cursos', function(){
+    return"Bienvenido a la pagina cursos";
+});
+//cursos/create
+Route:: get('cursos/create', function (){
+    return "En esta pagina podra crear un curso "; 
+ });
+ 
+/* Route::get('cursos/{curso}', function ($curso){
+    return"Bienvenido al curso: $curso";
+}); */
+Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+
+    if($categoria){
+        return "Bienvenido al curso $curso, de la categoria $categoria";
+    }else{
+        return"Bienvenido al curso: $curso";
+    }
+    
+    
 });
