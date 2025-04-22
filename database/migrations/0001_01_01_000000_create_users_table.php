7d8eae661eec936c209d19c112ca2e71f5ece693
tatan0989 <estefanyrojas200419@gmail.com>
2025-04-22 11:01:04 -0500

    Migraciones

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,13 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->id(); //Integer Unsigned Increment
+            $table->string('name'); //Vachar
+            $table->string('email')->unique();//Dato unico
+            $table->timestamp('email_verified_at')->nullable();//verificarcion de datos
+            $table->string('password');// Almacena contraseñas
+            $table->string('avatar');
+            $table->rememberToken();// crea columnas varchar de tamaño 100
+            $table->timestamps();// crea dos columnas craeted_at updated_at
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
